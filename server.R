a7afcb901d37d434691a7a455fa0edecaf26bda7
prabha <Prabhakaran.mails@gmail.com>
2021-06-16 14:31:56 -0400

    Updated the default date range to end of 2021 and changed the bar graph to line chart

--- server.R
+++ server.R
@@ -1,36 +1,45 @@
 library(ggplot2)
 library(scales)
 
-server <- function(input, output, session) {
-  
-  projectid = 'covid19-india-314022'
-  datasetid<-'covid19_india'
-  
-  bq_auth(path = "covid19-india-314022-7fe8b8ce467e.json")
-  
-  bq_conn <-  dbConnect(bigquery(), 
-                        project = projectid,
-                        dataset = datasetid, 
-                        use_legacy_sql = FALSE
-  )
-  
-  data <- tbl(bq_conn, "daily_covid19_india")
-  
-  output$covidTrend <- renderPlot({
 
+server <- function(input, output, session) {
+  retrive_data <- function() {
+    projectid = 'covid19-india-314022'
+    datasetid <- 'covid19_india'
     
-      data %>% 
-        filter(updated_date>=!!input$daterange[1] & updated_date <= !!input$daterange[2]) %>% 
-        ggplot(aes(x=updated_date,y=active_cases_total))+
-        geom_bar(stat='identity')+
-        facet_wrap(~state) +
-        scale_y_continuous(labels = unit_format(unit="k",scale=1e-3))+
-        theme_bw()+
-        theme(axis.title.x = element_blank(),
-              axis.title.y = element_blank(),
-              panel.spacing.y = unit(1,"lines")
-              )
+    bq_auth(path = "covid19-india-314022-7fe8b8ce467e.json")
+    bq_conn <-  dbConnect(
+      bigquery(),
+      project = projectid,
+      dataset = datasetid,
+      use_legacy_sql = FALSE
+    )
     
-  },width = "auto",height = 1000)
-  dbDisconnect(bq_conn)
+    data <- tbl(bq_conn, "daily_covid19_india") %>% as.data.frame
+    print("Data Downloaded from Big Query")
+    dbDisconnect(bq_conn)
+    return(data)
+  }
+  
+  
+  data = retrive_data()
+  
+  output$covidTrend  <- renderPlot({
+    data %>%
+      filter(updated_date >= !!input$daterange[1] &
+               updated_date <= !!input$daterange[2]) %>%
+      ggplot(aes(x = updated_date, y = active_cases_total)) +
+      geom_line(color = 'blue', size = 1) +
+      facet_wrap( ~ state, strip.position = "bottom") +
+      scale_y_continuous(labels = unit_format(unit = "k", scale = 1e-3)) +
+      theme_bw() +
+      theme (
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        panel.spacing.y = unit(1, "lines")
+      )
+    
+  }, width = "auto", height = 1000)
+  
+  
 }
--- ui.R
+++ ui.R
@@ -1,18 +1,21 @@
 library(shiny)
 library(tidyverse)
 library(bigrquery)
+library(shinycssloaders)
 
 ui <- fluidPage(
   # App title
   titlePanel("Covid19 Active Cases Trend in India"),
   
-  div(style="display:inline-block;vertical-align:top;float:right;",
-  fluidRow(
-    column(12,
-           dateRangeInput("daterange", "Date range:",
+  div(style = "display:inline-block;vertical-align:top;float:right;",
+      fluidRow(column(
+        12,
+        dateRangeInput("daterange", "Date range:",
                        start  = "2021-05-17",
-                     end   = "2021-05-31"),
-    ))),
-
-      plotOutput(outputId = "covidTrend")
+                       end   = "2021-12-31"),
+      ))),
+  
+  plotOutput(outputId = "covidTrend") %>% withSpinner(color = "#0dc5c1")
+  
+  
 )
